9168df596d472db446d25a519561da3c281e741d
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2025-04-22 16:47:57 -0400

    Initial commit

---
+++ test/assign4.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Assign4 from '../lib/assign4-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/assign4-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Assign4.Assign4Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
